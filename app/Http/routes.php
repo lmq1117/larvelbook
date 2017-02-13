a1b0a12890e988db2631e057cc0c9efeb53b77d8
LimgQuan <lmq1117@qq.com>
2017-02-13 11:48:16 +0800

    php storm de commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,9 +10,14 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+ use App\Entity\Member;
 Route::get('/', function () {
     return view('welcome');
+    // return Member::all();
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
 Route::get('/test', function () {
     return 'page test ...';
